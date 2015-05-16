a8d863beb324d15ee7b437365a27b53b39d56e4f
guolee89 <guolee89@163.com>
2015-05-16 16:48:55 +0800

    完成博客详情页bootstrap模板框架及简单示例

--- doit/settings.py
+++ doit/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'xadmin',
+    'crispy_forms',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
--- doit/urls.py
+++ doit/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
 
+# admin
 from django.contrib import admin
 admin.autodiscover()
 
+# xadmin
+import xadmin
+xadmin.autodiscover()
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'doit.views.home', name='home'),
+    url(r'^$', 'doit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^xadmin/', include(xadmin.site.urls)),
 )
